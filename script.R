3fb0647609caa47da1923d6bd68c936fc67a360c
higuchip <higuchip@gmail.com>
2021-11-27 21:42:57 -0300

    Inclusão de rotina de determinação de área basal

--- script.R
+++ script.R
@@ -1,2 +1,18 @@
-dados <- read.table('dados.csv', header=T, dec=',', sep=';')
+#Carregando pacote dplyr
+
+library(dplyr)
+
+#Importando os dados
+dados <- read.table('https://raw.githubusercontent.com/higuchip/blog/main/dados.csv', header=T, dec=',', sep=';')
+
+#Verificando os dados importados
 head(dados)
+
+#Determinação da Área Basal (m2)
+
+dados %>% 
+  mutate(as = dap^2/40000) %>% # determinação da Área seccional
+  group_by(Especie) %>%  # Agrupamento por espécies
+  summarise(ab=sum(as)) # Calculo da área basal (m2)
+
+
