ee9bdd50168e281ea1b475b156c0a7386f374517
higuchip <higuchip@gmail.com>
2022-02-02 11:21:55 -0300

    intersection added

---
+++ Python/intersection.py
@@ -0,0 +1,6 @@
+lista1 = ['Araucaria', 'Podocarpus', 'Myrcia', 'Eugenia']
+lista2 = ['Mimosa', 'Mysine', 'Araucaria', 'Podocarpus', 'Cedrella']
+
+intersecção = set(lista1).intersection(set(lista2))
+print(intersecção)
+print(len(intersecção))
